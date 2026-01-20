1d735d7351406d17e0eed661385d28259d3bdb8a
akbarKehidupan <akbridha@gmail.com>
2026-01-20 15:18:00 +0800

    menambah directory

---
+++ Object Repository/button.rs
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button</name>
+   <tag></tag>
+   <elementGuidId>2284b4e6-c229-4077-a5fa-b5f967822234</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/button_a.rs
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_a</name>
+   <tag></tag>
+   <elementGuidId>6ec883b5-59e0-4697-ac8b-1bf124469608</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
